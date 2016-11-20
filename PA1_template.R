1dd7c2c73c22d0bd2d011c5025495538d6f8b613
PCE <pete.earle@gmail.com>
2016-11-20 15:02:04 -0500

    Add files via upload

---
+++ PA1_template.R
@@ -0,0 +1,107 @@
+##### PART ONE 
+
+# Read activity.csv file into "activity" variable
+activity <- read.csv(file="C:/Users/Owner/datasciencecoursera/activity.csv")
+
+# Convert date field from factor to date
+activity$date <- as.Date(activity$date)
+
+
+# Load reshape2 library to get melt & dcast functions
+library(reshape2)
+
+# Melt data frame to prep for casting by date
+actMeltDate <- melt(activity, id.vars="date", measure.vars="steps", na.rm=FALSE)
+
+# Cast data frame to see steps per day -- this sums the steps by date to give us a table of 3 columns x 61 rows
+actCastDate <- dcast(actMeltDate, date ~ variable, sum)
+Step 1C: Plot histogram of data
+
+# Plot histogram with frequency of steps by day and add a red line showing the mean value
+plot(actCastDate$date, actCastDate$steps, type="h", main="Histogram of Daily Steps", xlab="Date", ylab="Steps per Day", col="blue", lwd=8)
+abline(h=mean(actCastDate$steps, na.rm=TRUE), col="red", lwd=2)
+
+# Calculate mean and median of daily steps
+paste("Mean Steps per Day =", mean(actCastDate$steps, na.rm=TRUE))
+
+# "Mean Steps per Day = 10766.1886792453"
+paste("Median Steps per Day =", median(actCastDate$steps, na.rm=TRUE))
+
+#####PART TWO
+
+# Re-melt data frame to prep for casting by interval, including removing NA values so we can take the mean a little later
+actMeltInt <- melt(activity, id.vars="interval", measure.vars="steps", na.rm=TRUE)
+
+# Cast data frame to see mean steps per interval
+actCastInt <- dcast(actMeltInt, interval ~ variable, mean)
+
+# The plot shows the peak at somewhere in the 800-900 interval range so let's find out exactly which interval has the max value and what that maximum value is
+# Output interval that has max value along with the max value
+paste("Interval with max value =", actCastInt$interval[which(actCastInt$steps == max(actCastInt$steps))])
+
+paste("Maximum interval mean steps =", max(actCastInt$steps))
+
+#####PART THREE
+
+# Calculate number of rows in activity data set with NA rows
+sum(is.na(activity$steps))
+
+# Data frame with mean steps per interval - just renaming to be more descriptive
+stepsPerInt <- actCastInt
+
+# Create data frame that we will remove NAs from
+actNoNA <- activity
+
+# Merge activity data set with stepsPerInt data set
+actMerge = merge(actNoNA, stepsPerInt, by="interval", suffixes=c(".act", ".spi"))
+
+# Get list of indexes where steps value = NA
+naIndex = which(is.na(actNoNA$steps))
+
+# Replace NA values with value from steps.spi
+actNoNA[naIndex,"steps"] = actMerge[naIndex,"steps.spi"]
+
+# Melt new data frame to prep for casting by date
+actMeltDateNoNA <- melt(actNoNA, id.vars="date", measure.vars="steps", na.rm=FALSE)
+
+# Cast data frame to see steps per day
+actCastDateNoNA <- dcast(actMeltDateNoNA, date ~ variable, sum)
+
+# Plot histogram with frequency of steps by day
+plot(actCastDateNoNA$date, actCastDateNoNA$steps, type="h", main="Histogram of Daily Steps (Imputted NA Values)", xlab="Date", ylab="Steps", col="gray", lwd=8)
+abline(h=mean(actCastDateNoNA$steps), col="red", lwd=2)
+
+# Calculate mean and median of daily steps
+paste("Mean daily steps =", mean(actCastDateNoNA$steps, na.rm=TRUE))
+paste("Median daily steps =", median(actCastDateNoNA$steps, na.rm=TRUE))
+
+#####PART FOUR
+
+# For loop to create new column called "dayOfWeek" and insert whether each date corresponds to a weekday or weekend
+for (i in 1:nrow(actNoNA)) {
+    if (weekdays(actNoNA$date[i]) == "Saturday" | weekdays(actNoNA$date[i]) == "Sunday") {
+        actNoNA$dayOfWeek[i] = "weekend"
+    } else {
+        actNoNA$dayOfWeek[i] = "weekday"
+    }
+}
+
+# To create a plot, we must first subset the data
+actWeekday <- subset(actNoNA, dayOfWeek=="weekday")
+actWeekend <- subset(actNoNA, dayOfWeek=="weekend")
+
+# Next, we need to process the data for our needs
+actMeltWeekday <- melt(actWeekday, id.vars="interval", measure.vars="steps")
+actMeltWeekend <- melt(actWeekend, id.vars="interval", measure.vars="steps")
+actCastWeekday <- dcast(actMeltWeekday, interval ~ variable, mean)
+actCastWeekend <- dcast(actMeltWeekend, interval ~ variable, mean)
+
+# Load plot packages necessary to continue
+library(ggplot2)
+library(gridExtra)
+
+# Set plot area to two rows and one column, and then plot charts with mean line in each
+plot1 <- qplot(actCastWeekday$interval, actCastWeekday$steps, geom="line", data=actCastWeekday, type="bar", main="Steps by Interval - Weekday", xlab="Interval ID", ylab="Number of Steps")
+plot2 <- qplot(actCastWeekend$interval, actCastWeekend$steps, geom="line", data=actCastWeekend, type="bar", main="Steps by Interval - Weekend", xlab="Interval ID", ylab="Number of Steps")
+grid.arrange(plot1, plot2, nrow=2)
+
